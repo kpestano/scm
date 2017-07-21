a404e99bec5844a071a70e6db764156ef1c885c3
Timothy Penarubia <Timothy.Penarubia@crealogix.com>
2017-07-21 12:01:40 +0800

    fixed spring security

--- src/main/java/com/sofgen/scm/configuration/SecurityConfiguration.java
+++ src/main/java/com/sofgen/scm/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  * @author tpenarubia
  *
  */
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
