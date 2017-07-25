e2ab2a24283fff399fa394ed699863e29237754c
Kevin Jay Pestano <KevinJay.Pestano@crealogix.com>
2017-07-25 10:18:44 +0800

    revert changes

--- src/main/java/com/sofgen/scm/configuration/WebMvcConfig.java
+++ src/main/java/com/sofgen/scm/configuration/WebMvcConfig.java
@@ -19,14 +19,4 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
 		return bCryptPasswordEncoder;
 	}
 	
-	/*private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/static/", "classpath:/public/"
-    };
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-    }*/
 }
